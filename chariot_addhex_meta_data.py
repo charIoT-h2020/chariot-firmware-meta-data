bef40e50bf592a00893ad08a66263a719f2ed80e
Franck Vedrine <franck.vedrine@cea.fr>
2019-09-22 13:03:59 +0200

    correction of chariot_addhex_meta_data.py

--- chariot_addhex_meta_data.py
+++ chariot_addhex_meta_data.py
@@ -19,7 +19,7 @@ def close_fd_and_file(*args, **kwargs):
 
 def compute_sha_256(in_file_name, verbose):
     sha_256_proc = subprocess.Popen(['sha256sum', in_file_name], stdout=subprocess.PIPE)
-    sha_result = sha_256_proc.stdout.read().partition(' ')[0]
+    sha_result = sha_256_proc.stdout.read().decode("utf-8").partition(' ')[0]
     returncode = sha_256_proc.wait()
     if verbose or returncode:
         command = "sha256sum " + in_file_name
@@ -27,22 +27,25 @@ def compute_sha_256(in_file_name, verbose):
             print ("[error] the command " + command + " has failed with return code " + str(returncode))
             raise OSError(returncode)
         print (command)
-    return sha_result
+    return bytes.fromhex(sha_result)
 
 def compute_git_version(in_file_name, verbose):
     git_log_proc = subprocess.Popen(
             ['git', 'log', '--format=oneline', '--abbrev=12', '--abbrev-commit', '-q',
                 in_file_name], stdout=subprocess.PIPE)
-    git_version_result = git_log_proc.stdout.read().partition(' ')[0]
+    git_version_result = git_log_proc.stdout.read().decode("utf-8").partition(' ')[0]
     returncode = git_log_proc.wait()
-    if verbose or returncode:
+    if verbose or returncode or (len(git_version_result) == 0):
         command = "git log --format=oneline --abbrev=12 --abbrev-commit -q " + in_file_name + ""
-        if returncode:
-            print ("[warning] the command " + command + " has failed with return code " + str(returncode))
+        if returncode or (len(git_version_result) == 0):
+            if returncode:
+                print ("[warning] the command " + command + " has failed with return code " + str(returncode))
+            elif verbose:
+                print ("[warning] the command " + command + " has not returned any result")
             git_version_result = "0000000000000000000000000000000000000000000000000000000000000000"
         else:
             print (command)
-    return git_version_result
+    return bytes.fromhex(git_version_result)
 
 parser = argparse.ArgumentParser(description='Add Chariot meta-data into an hex firmware')
 parser.add_argument('hex_name', help='the name of the executable hex file')
@@ -101,7 +104,7 @@ try:
                 total_size += hexm_file.write(buf)
             else:
                 break
-        os.close(hex_file);
+        hex_file.close();
 
         add_size = 0;
         if args.all is not None:
@@ -114,40 +117,38 @@ try:
                         add_size += hexm_file.write(buf)
                     else:
                         break
-                os.close(meta_file);
+                meta_file.close();
         else:
             if args.verbose:
                 print ("generate meta-data section") 
-            hexm_file.write(":chariot_md:")
+            hexm_file.write(":chariot_md:".encode())
             add_size += len(":chariot_md:")
             if args.verbose:
                 print ("generate sha256 of " + args.hex_name)
-            hexm_file.write(":sha256:")
+            hexm_file.write(":sha256:".encode())
             add_size += len(":sha256:")
-            hexm_file.write(
-                compute_sha_256_content(args.hex_name, verbose)
-                    .to_bytes(256, byteorder='little', signed=False))
-            add_size += 256/8
+            hexm_file.write(compute_sha_256(args.hex_name, args.verbose))
+            add_size += int(256/8)
             if args.verbose:
                 print ("generate chariot format")
-            hexm_file.write(":fmt:")
+            hexm_file.write(":fmt:".encode())
             add_size += len(":fmt:")
             hexm_file.write(
                 (len("!CHARIOTMETAFORMAT_2019a"))
-                    .to_bytes(32, byteorder='little', signed=False))
-            add_size += 32/8
-            hexm_file.write("!CHARIOTMETAFORMAT_2019a")
+                    .to_bytes(4, byteorder='big', signed=False))
+            add_size += 4
+            hexm_file.write("!CHARIOTMETAFORMAT_2019a".encode())
             add_size += len("!CHARIOTMETAFORMAT_2019a")
 
             if additional_data_file is not None:
                 if args.verbose:
                     print ("add additional file " + additional_data_file)
-                hexm_file.write(":add:")
+                hexm_file.write(":add:".encode())
                 add_size += len(":add:")
                 hexm_file.write(
                     (os.path.getsize(additional_data_file))
-                        .to_bytes(32, byteorder='little', signed=False))
-                add_size += 32/8
+                        .to_bytes(4, byteorder='big', signed=False))
+                add_size += 4
                 with open(additional_data_file,'rb') as add_file:
                     while True:
                         buf=add_file.read(4096)
@@ -155,58 +156,56 @@ try:
                             add_size += hexm_file.write(buf)
                         else:
                             break
-                    os.close(add_file);
-                hexm_file.write(':')
+                    add_file.close();
+                hexm_file.write(":".encode())
                 add_size += 1
                 hexm_file.write(
                     (len(additional_data_mime))
-                        .to_bytes(32, byteorder='little', signed=False))
-                add_size += 32/8
-                hexm_file.write(additional_data_mime)
+                        .to_bytes(4, byteorder='big', signed=False))
+                add_size += 4
+                hexm_file.write(additional_data_mime.encode())
                 add_size += len(additional_data_mime)
 
             if args.verbose:
                 print ("generate version of " + args.hex_name)
-            hexm_file.write(":version:")
+            hexm_file.write(":version:".encode())
             add_size += len(":version:")
-            hexm_file.write(
-                compute_git_version(args.hex_name, verbose)
-                    .to_bytes(256, byteorder='little', signed=False))
-            add_size += 256/8
+            hexm_file.write(compute_git_version(args.hex_name, args.verbose))
+            add_size += int(256/8)
 
-            if block_chain_path is not None:
+            if blockchain_path is not None:
                 if args.verbose:
                     print ("add blockchain path")
-                hexm_file.write(":bcpath:")
+                hexm_file.write(":bcpath:".encode())
                 add_size += len(":bcpath:")
                 hexm_file.write(
-                    (len(block_chain_path))
-                        .to_bytes(32, byteorder='little', signed=False))
-                add_size += 32/8
-                hexm_file.write(block_chain_path)
-                add_size += len(block_chain_path)
+                    (len(blockchain_path))
+                        .to_bytes(4, byteorder='big', signed=False))
+                add_size += 4
+                hexm_file.write(blockchain_path.encode())
+                add_size += len(blockchain_path)
 
-            if licence is not None:
+            if license is not None:
                 if args.verbose:
-                    print ("add licence")
-                hexm_file.write(":lic:")
+                    print ("add license")
+                hexm_file.write(":lic:".encode())
                 add_size += len(":lic:")
                 hexm_file.write(
-                    (len(licence))
-                        .to_bytes(32, byteorder='little', signed=False))
-                add_size += 32/8
-                hexm_file.write(licence)
-                add_size += len(licence)
+                    (len(license))
+                        .to_bytes(4, byteorder='big', signed=False))
+                add_size += 4
+                hexm_file.write(license.encode())
+                add_size += len(license)
 
             if static_code_analysis_file is not None:
                 if args.verbose:
                     print ("add static analysis results")
-                hexm_file.write(":sca:")
+                hexm_file.write(":sca:".encode())
                 add_size += len(":sca:")
                 hexm_file.write(
                     (os.path.getsize(static_code_analysis_file))
-                        .to_bytes(32, byteorder='little', signed=False))
-                add_size += 32/8
+                        .to_bytes(4, byteorder='big', signed=False))
+                add_size += 4
                 with open(static_code_analysis_file,'rb') as add_file:
                     while True:
                         buf=add_file.read(4096)
@@ -214,29 +213,29 @@ try:
                             add_size += hexm_file.write(buf)
                         else:
                             break
-                    os.close(add_file);
-                hexm_file.write(':')
+                    add_file.close();
+                hexm_file.write(":".encode())
                 add_size += 1
                 hexm_file.write(
                     (len(static_code_analysis_mime))
-                        .to_bytes(32, byteorder='little', signed=False))
-                add_size += 32/8
-                hexm_file.write(static_code_analysis_mime)
+                        .to_bytes(4, byteorder='big', signed=False))
+                add_size += 4
+                hexm_file.write(static_code_analysis_mime.encode())
                 add_size += len(static_code_analysis_mime)
 
             if args.verbose:
                 print ("add additional size info")
-            add_size += 32/8
+            add_size += 4
             hexm_file.write(
-               (add_size).to_bytes(32, byteorder='little', signed=False))
+               (add_size).to_bytes(4, byteorder='big', signed=False))
 
         if args.verbose:
             print ("close file and check")
-        os.close(hexm_file);
-        if os.path.getsize(hexm_file) != add_size + total_size:
+        hexm_file.close();
+        if os.path.getsize(output_file) != add_size + total_size:
             print ("[error] internal size computation is not correct: "
-                    + "file size = " + str(os.path.getsize(hexm_file))
-                    + ", original size = " + str(total_size) +
+                    + "file size = " + str(os.path.getsize(output_file))
+                    + ", original size = " + str(total_size)
                     + ", additional size = " + str(add_size))
             raise OSError(1)
         if args.verbose:
