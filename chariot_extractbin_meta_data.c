e531dbe7b3425c595e1f737fea8f9e4daae93b18
Franck Vedrine <franck.vedrine@cea.fr>
2019-09-25 00:31:08 +0200

    first version of chariot_extractbin_meta_data.c

---
+++ chariot_extractbin_meta_data.c
@@ -0,0 +1,250 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+#include <string.h>
+#include <stdbool.h>
+#include <errno.h>
+
+typedef struct _InputParser {
+  const char* exe_name;
+  bool requires_help : 1;
+  bool requires_all : 1;
+  bool requires_verbose : 1;
+  bool requires_sha : 1;
+  bool requires_format : 1;
+  bool requires_blockchain_path : 1;
+  bool requires_license : 1;
+  bool requires_static_analysis : 1;
+  bool requires_additional : 1;
+  const char* output_file;
+} InputParser;
+
+int
+standard_error(FILE* out_file, FILE* hexm_file, InputParser* parser) {
+  printf("original file %s has not expected hybrid format\n", parser->exe_name);
+  if (out_file) fclose(out_file);
+  fclose(hexm_file);
+  return 1;
+}
+
+void
+ensure_endianness(u_int32_t* value) {
+  int hostEndianness = 0x1234;
+  if (*(char*)(&hostEndianness) == 0x34) { // little endian
+    u_int32_t res = 0;
+    res |= (*value & 0xff) << 24;
+    res |= ((*value >> 8) & 0xff) << 16;
+    res |= ((*value >> 16) & 0xff) << 8;
+    res |= ((*value >> 24) & 0xff);
+    *value = res;
+  }
+}
+
+void
+input_parser_usage()
+{
+  printf("usage: chariot_extractbin_meta_data [-h] [--all] [--verbose] [--sha]\n"
+         "                                    [--blockchain_path] [--license]\n"
+         "                                    [--static-analysis] [--add]\n"
+         "                                    [--format] [--output OUTPUT]\n"
+         "                                    exe_name\n"
+         "\n");
+}
+
+bool
+fill_input_parser_fields(InputParser* parser, int argc, const char** argv)
+{
+  memset(parser, 0, sizeof(InputParser));
+  for (int i = 1; i < argc; ++i)
+  {
+    if (argv[i][0] == '-')
+    {
+      if (strcmp(argv[i], "-h") == 0)
+        parser->requires_help = true;
+      else if (strcmp(argv[i], "-a") == 0 || strcmp(argv[i], "--all") == 0)
+        parser->requires_all = true;
+      else if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0)
+        parser->requires_verbose = true;
+      else if (strcmp(argv[i], "-sha") == 0 || strcmp(argv[i], "--sha") == 0)
+        parser->requires_sha = true;
+      else if (strcmp(argv[i], "-format") == 0 || strcmp(argv[i], "--format") == 0)
+        parser->requires_format = true;
+      else if (strcmp(argv[i], "-bp") == 0 || strcmp(argv[i], "--blockchain_path") == 0)
+        parser->requires_blockchain_path = true;
+      else if (strcmp(argv[i], "-lic") == 0 || strcmp(argv[i], "--license") == 0)
+        parser->requires_license = true;
+      else if (strcmp(argv[i], "-sa") == 0 || strcmp(argv[i], "--static-analysis") == 0)
+        parser->requires_static_analysis = true;
+      else if (strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "--output") == 0)
+      {
+        if (++i >= argc)
+          return false;
+        parser->output_file = argv[i];
+      }
+      else
+        return false;
+    }
+    else
+      parser->exe_name = argv[i];
+  }
+  if (!parser->exe_name || strlen(parser->exe_name) == 0)
+    return parser->requires_help;
+  return true;
+}
+
+int main(int argc, const char** argv) {
+  InputParser parser;
+  if (!fill_input_parser_fields(&parser, argc, argv))
+  {
+    input_parser_usage();
+    return 1;
+  }
+
+  if (parser.requires_help)
+  {
+    input_parser_usage();
+    printf("\n"
+           "Extract Chariot meta-data from a bin firmware\n"
+           "\n"
+           "positional arguments:\n"
+           "  exe_name              the name of the executable elf file\n"
+           "\n"
+           "optional arguments:\n"
+           "  -h, --help            show this help message and exit\n"
+           "  --all, -a             equivalent to -sha -sa -bp -lic -add\n"
+           "  --verbose, -v         verbose mode: echo every command on terminal\n"
+           "  --sha, -sha           print the sha256 of the bin file\n"
+           "  --format, -format     print the format\n"
+           "  --blockchain_path, -bp\n"
+           "                        print the path to the targeted blockchain\n"
+           "  --license, -lic       print the license of the firmware\n"
+           "  --static-analysis, -sa\n"
+           "                        print the result of the static analysis as file/format\n"
+           "  --add, -add           print content of the additional section\n"
+           "  --output OUTPUT, -o OUTPUT\n"
+           "                        print into the output file instead of stdout\n"
+           "\n");
+    return 0;
+  }
+
+  FILE* out_file = NULL;
+  bool is_valid_out_file = false;
+  if (parser.output_file && strlen(parser.output_file) > 0)
+  {
+    out_file = fopen(parser.output_file, "w");
+    is_valid_out_file = out_file != NULL;
+  }
+  if (!is_valid_out_file)
+    out_file = stdout;
+
+  FILE* hexm_file = fopen(parser.exe_name, "rb");
+  if (!hexm_file)
+  {
+    fprintf(stderr, "Cannot open file %s\n", parser.exe_name);
+    return 1;
+  }
+  u_int32_t metadata_size = 0;
+  if (fseek(hexm_file, -4, SEEK_END)) {
+    printf("impossible to find metadata, error code = %d", errno);
+    return 1;
+  }
+  if (fread(&metadata_size, 1, 4, hexm_file) != 4) {
+    printf("impossible to find metadata, error code = %d", errno);
+    return 1;
+  }
+  ensure_endianness(&metadata_size);
+  if (fseek(hexm_file, -(long) metadata_size, SEEK_END)) {
+    printf("impossible to find metadata, error code = %d", errno);
+    return 1;
+  }
+
+  if (parser.requires_all) {
+    if (!out_file) {
+      printf("extraction of all meta-data requires an output file\n");
+      if (out_file) fclose(out_file);
+      fclose(hexm_file);
+      return 1;
+    }
+    char buffer[4096];
+    while (true) {
+      int len = fread(buffer, 1, 4096, hexm_file);
+      if (len > 0)
+        fwrite(buffer, 1, len, out_file);
+      else
+        break;
+    }
+    if (out_file) fclose(out_file);
+    fclose(hexm_file);
+    return 0;
+  }
+
+  if (parser.requires_verbose)
+    printf("extract meta-data section\n");
+  char buffer[100]; 
+  int len = fread(buffer, 1, strlen(":chariot_md:"), hexm_file);
+  buffer[len] = '\0';
+  if (strcmp(buffer, ":chariot_md:") != 0)
+    return standard_error(out_file, hexm_file, &parser);
+  if (parser.requires_sha && parser.requires_verbose)
+    printf("extract sha256 of %s\n", parser.exe_name);
+  len = fread(buffer, 1, strlen(":sha256:"), hexm_file);
+  buffer[len] = '\0';
+  if (strcmp(buffer, ":sha256:") != 0)
+    return standard_error(out_file, hexm_file, &parser);
+
+  len = fread(buffer, 1, 256/8, hexm_file);
+  if (len != 256/8)
+    return standard_error(out_file, hexm_file, &parser);
+  if (parser.requires_sha) {
+    for (int i = 256/8-1; i >= 0; --i) {
+      int val = buffer[i] & 0xf;
+      buffer[2*i+1] = (val >= 10) ? (char) (val-10+'a') : (char) (val+'0');
+      val = (buffer[i] >> 4) & 0xf;
+      buffer[2*i] = (val >= 10) ? (char) (val-10+'a') : (char) (val+'0');
+    }
+    buffer[256/4] = '\n',
+    fwrite(buffer, 1, 256/4+1, out_file);
+  }
+
+  if (parser.requires_verbose && parser.requires_format)
+    printf("extract chariot format");
+  len = fread(buffer, 1, strlen(":fmt:"), hexm_file);
+  buffer[len] = '\0';
+  if (strcmp(buffer, ":fmt:") != 0)
+    return standard_error(out_file, hexm_file, &parser);
+  u_int32_t fmt_size = 0;
+  if (fread(&fmt_size, 1, 4, hexm_file) != 4)
+    return standard_error(out_file, hexm_file, &parser);
+  ensure_endianness(&fmt_size);
+  { char buffer[4096];
+    do {
+      u_int32_t size = fmt_size > 4096 ? 4096 : fmt_size;
+      len = fread(buffer, 1, size, hexm_file);
+      if (len != size)
+        return standard_error(out_file, hexm_file, &parser);
+      if (parser.requires_format)
+        fwrite(buffer, 1, size, out_file);
+      fmt_size -= size;
+    } while (fmt_size > 0);
+  }
+
+/*
+  if (fgetc(hexm_file) != ':')
+    return standard_error(out_file, hexm_file, &parser);
+  int index = 0;
+  { char ch;
+    while ((ch = fgetc(hexm_file) != ':') && ch != '\0' && index < 100-1)
+      buffer[index++] = ch;
+    if (ch == '\0' || index >= 100-1)
+      return standard_error(out_file, hexm_file, &parser);
+    buffer[index] = '\0';
+  }
+
+  if (strcmp(buffer, "add") == 0)
+*/
+
+  if (out_file) fclose(out_file);
+  fclose(hexm_file);
+  return 0;
+}
+
--- makefile
+++ makefile
@@ -1,6 +1,5 @@
-lib: libchariot_extractelf.a
-
 all: lib exe
+lib: libchariot_extractelf.a
 
 CFLAGS=-O2 -Wall
 # CFLAGS=-g -O0
@@ -12,10 +11,13 @@ libchariot_extractelf.a : chariot_extractelf.o
 chariot_extractelf.o: chariot_extractelf.c chariot_extractelf.h elf32.h
 	gcc $(CFLAGS) -c $< -o $@
 
-exe: chariot_extractelf_meta_data.exe
+exe: chariot_extractelf_meta_data.exe chariot_extractbin_meta_data.exe
 
 chariot_extractelf_meta_data.exe: chariot_extractelf_meta_data.c libchariot_extractelf.a
 	gcc $(CFLAGS) $< -o $@ -L. -lchariot_extractelf
+
+chariot_extractbin_meta_data.exe: chariot_extractbin_meta_data.c
+	gcc $(CFLAGS) $< -o $@
 
 # chariot_extractelf_meta_data.exe: chariot_extractelf_meta_data.cpp libchariot_extractelf.a
 #	g++ -std=c++14 $(CFLAGS) $< -o $@ -L. -lchariot_extractelf
