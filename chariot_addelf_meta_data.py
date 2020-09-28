195f9f513fe1aa32e4d83d7ed378203d7c60e8fc
Franck Vedrine <franck.vedrine@cea.fr>
2020-09-28 13:27:20 +0200

    add license files

--- chariot_addelf_meta_data.py
+++ chariot_addelf_meta_data.py
@@ -5,6 +5,45 @@ import argparse
 import subprocess
 import tempfile
 
+__author__ = "Franck Vedrine"
+__copyright__ = "Copyright (c) 2019-2020, Commissariat a l'Energie Atomique CEA. All rights reserved."
+__credits__ = ["""
+    European Union’s Horizon 2020 RIA programme
+    under grant agreement No 780075
+    CHARIOT - Cognitive Heterogeneous Architecture for Industrial IoT
+    """]
+__license__ = """
+  Redistribution and use in source and binary forms, with or without 
+  modification, are permitted provided that the following conditions are met:
+
+   - Redistributions of source code must retain the above copyright notice, 
+     this list of conditions and the following disclaimer.
+
+   - Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+
+   - Neither the name of CEA nor the names of its contributors may be used to
+     endorse or promote products derived from this software without specific 
+     prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+  ARE DISCLAIMED.
+  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY 
+  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
+  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND 
+  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF 
+  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+__version__ = "1.0.0"
+__maintainer__ = "Franck Vedrine"
+__email__ = "franck.vedrine@cea.fr"
+__status__ = "Prototype"
+
 # CHARIOT: Add meta-data into firmware
 # requires size, objcopy supporting add-section option
 #          gcc (at least gnu-as)
